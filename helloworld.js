fca4f023de0b23c0b65d860786592c8b315d40bc
agakpe <49899548+agakpe@users.noreply.github.com>
2024-01-30 23:31:28 -0500

    Update helloworld.js

--- helloworld.js
+++ helloworld.js
@@ -8,7 +8,7 @@ http.createServer(function (request, response) {
    response.writeHead(200, {'Content-Type': 'text/plain'})
 
    // Send the response body as "Hello World"
-   response.end('Hello World\n')
+   response.end('Hello World Vampire\n Where are all the people? ')
 }).listen(3333)
 
 // Console will print the message
